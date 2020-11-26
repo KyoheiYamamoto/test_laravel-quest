8ff24fc2679cc901c355637d49df94f8c6f661a8
KyoheiYamamoto <arienkyohei@gmail.com>
2020-11-26 04:56:34 +0000

    show/destroyアクションの実装

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User; 
+use App\Movie; 
+
 
 class MoviesController extends Controller
 {
@@ -46,11 +49,13 @@ class MoviesController extends Controller
             'url' => 'required|max:11',
             'comment' => 'max:36',
         ]);
-        
-        $request->user()->movies->create([
-         'url' => $request->url,
-         'comment' => $request->comment,
+
+        $request->user()->movies()->create([
+            'url' => $request->url,
+            'comment' => $request->comment,
         ]);
+
+        return back();
 
     }
 
@@ -96,6 +101,12 @@ class MoviesController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $movie = Movie::find($id);
+        
+        if(\Auth::id() == $movie->user_id){
+            $movie->delete();
+        }
+        
+        return back();
     }
 }
